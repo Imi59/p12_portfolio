5c6f1bc68ff63421d737974560b84d107fdb1be4
Imane Baqua <imane.baqua@hotmail.fr>
2024-06-13 14:02:40 +0200

    composant skills ok

--- portfolio/src/components/Skills/Skills.jsx
+++ portfolio/src/components/Skills/Skills.jsx
@@ -39,6 +39,33 @@ function Skills() {
           className="skills-section__container__img"
         />
       </div>
+      <div className="skills__list">
+        <div className="skills__category">
+          <h3>Outils</h3>
+          <ul>
+            <li>
+              <i className="fab fa-git-alt"></i> Git & GitHub
+            </li>
+            <li>
+              <i className="fas fa-database"></i> MongoDB
+            </li>
+            <li>
+              <i className="fas fa-cogs"></i> Redux
+            </li>
+            <li>
+              <i className="fas fa-laptop-code"></i> React Developer Tools
+            </li>
+          </ul>
+        </div>
+        <div className="skills__category">
+          <h3>Design</h3>
+          <ul>
+            <li>
+              <i className="fab fa-figma"></i> Figma
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
