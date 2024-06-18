e448e8eda4af042501b0f4808c46f2c20558afb8
Imane Baqua <imane.baqua@hotmail.fr>
2024-06-18 18:30:05 +0200

    maj package json

--- portfolio/src/pages/About/About.jsx
+++ portfolio/src/pages/About/About.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "../../components/Button/Button";
 
 const About = () => {
-  const handleGoBack = () => {
-    // Redirection vers la page d'accueil lorsque la "maison" est cliquée
-    window.location.href = "/";
-  };
-
   return (
     <div id="about" className="about">
-      <Link className="go-back" onClick={handleGoBack} to="/">
+      <Link className="go-back" to="/">
         <i className="fa fa-home"></i>
       </Link>
 
@@ -23,12 +17,12 @@ const About = () => {
           Formée par OpenClassRooms via leur formation "Intégrateur Web".
           <br />
           Passionnée par le monde du Web, j'ai décidé de changer d'horizons et
-          passer de l'Ingénierie Logistique au Dévellopement Web.
+          passer de l'Ingénierie Logistique au Développement Web.
           <br />
-          Ma formation professionelle m'a permis de travailler sur une variété
+          Ma formation professionnelle m'a permis de travailler sur une variété
           de projets, allant de sites web "simples" à des applications
           d'entreprise bien plus complexes me permettant d'acquérir de
-          nombreuses compétences téchnologiques que vous pourrez découvrir dans
+          nombreuses compétences technologiques que vous pourrez découvrir dans
           les différentes rubriques de mon portfolio.
           <br />
           <br />
